d5069fcce229fc016f154d1cc3f240b0125dd251
vaelixd <vaelixd@proton.me>
2025-11-23 22:02:36 +1300

    add new config options
    New options:
    - kernel
    - bootloader
    - initramfsGenerator
    - preBuild
    - postBuild

--- nest.py
+++ nest.py
@@ -1,15 +1,22 @@
 from dataclasses import asdict, dataclass
+from inspect import getsource
+from os import getenv, mkdir
+from types import FunctionType
+from typing import Optional
 
 @dataclass
 class NestConfig:
     hostname: str
+    kernel: str = "linux"
+    bootloader: str = "limine"
+    initramfsGenerator: str = "booster"
+    preBuild: Optional[FunctionType] = None
+    postBuild: Optional[FunctionType] = None
+
+os_info = {}
+nest_gen_root = getenv("NEST_GEN_ROOT") or ""
 
 def newConfig() -> NestConfig:
-    os_info = {}
-    config = NestConfig(
-        hostname=""
-    )
-
     with open("/etc/os-release", "r") as os_release:
         for line in os_release:
             line = line.strip()
@@ -19,7 +26,9 @@ def newConfig() -> NestConfig:
             value = value.strip("'\"")
             os_info[key.lower()] = value
 
-    config.hostname = os_info["id"]
+    config = NestConfig(
+        hostname=os_info["id"],
+    )
 
     return config
 
@@ -28,12 +37,27 @@ def returnConfig(config: NestConfig):
     scsvConfig = ""
 
     for key in configDict:
-        scsvConfig += key + "," + __checkValue(key, configDict[key]) + "\n"
+        value = __checkValue(key, configDict[key])
+        if value == False:
+            continue
+        scsvConfig += key + "," + value + "\n"
 
     print(scsvConfig)
 
 def __checkValue(key: str, value):
     if key == "hostname":
         return str(value).replace(" ", "-")
+    if key == "preBuild" or key == "postBuild":
+        if value != None:
+            buildFunc = [getsource(value), value.__name__]
+            __generateBuildFiles(buildFunc, key)
+
+        return False
     else:
         return value
+
+def __generateBuildFiles(buildFunc: list[str], buildType: str):
+    mkdir(nest_gen_root)
+
+    with open(nest_gen_root + buildType + ".py", "w") as file:
+            file.writelines([buildFunc[0] + "\n", buildFunc[1] + "()"])
