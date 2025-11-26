b6200ef2597e3e7c10b9712b8c7640db280f2678
vaelixd <vaelixd@proton.me>
2025-11-26 19:51:08 +1300

    update example

--- examples/config.py
+++ examples/config.py
@@ -5,27 +5,36 @@ def genInfo():
     with open(f"{nest.nest_autogen}genInfo.md", "w") as file:
         file.write("Hello")
 
+
 def preBuild():
     pass
+
 
 def postBuild():
     genInfo()
 
+
 config = nest.newConfig()
 
 config.hostname = "vaelixd-pc"
+config.timezone = "Australia/Sydney"
 config.kernels = ["linux-zen", "linux", "linux-lts"]
 config.bootloader = "refind"
 config.initramfsGenerator = "dracut"
 config.preBuild = preBuild
 config.postBuild = postBuild
 
+config.locale = nest.Locale(
+    lang="en_US.UTF-8",
+    address="en_AU.UTF-8",
+)
+
 vaelixd = nest.User(
     fullName="vaelixd",
     userName="vaelixd",
     homeDir="/home/vaelixd",
     manageHome=True,
-    groups=["wheel"]
+    groups=["wheel"],
 )
 
 dds = nest.User(
@@ -34,7 +43,7 @@ dds = nest.User(
 
 config.users = [
     vaelixd,
-    dds
+    dds,
 ]
 
 nest.returnConfig(config)
