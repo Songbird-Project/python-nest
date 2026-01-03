6b02f9ce7a2251c3b0461c28a00f3b4cdb5ee59b
vaelixd <vaelixd@proton.me>
2026-01-04 10:57:23 +1300

    remove intermediate data structures

--- nest.py
+++ nest.py
@@ -1,160 +1,98 @@
-from dataclasses import asdict, dataclass
+from dataclasses import asdict, dataclass, field
 from os import getenv, path
 from pathlib import Path
 from json import dump, dumps
 
 
 @dataclass
-class _LocaleConfig:
-    lang: str
-    address: str
-    identification: str
-    measurement: str
-    monetary: str
-    name: str
-    numeric: str
-    paper: str
-    telephone: str
-    time: str
-
-
-@dataclass
-class _UserConfig:
-    username: str
-    fullname: str
-    home_dir: str
-    shell: str
-    manage_home: bool
-    groups: list[str]
-
-
-@dataclass
-class _SystemConfig:
-    hostname: str
-    timezone: str
-    locale: dict
-    kernels: list[str]
-    users: list[dict]
-    bootloader: str
-    initramfs_generator: str
-    pre_build: str
-    post_build: str
-
-
 class Locale:
-    def __init__(
-        self,
-        lang: str = "",
-        address: str = "",
-        identification: str = "",
-        measurement: str = "",
-        monetary: str = "",
-        name: str = "",
-        numeric: str = "",
-        paper: str = "",
-        telephone: str = "",
-        time: str = "",
-    ):
-        if not lang and not address:
-            address = "en_US.UTF-8"
+    lang: str = ""
+    address: str = ""
+    identification: str = ""
+    measurement: str = ""
+    monetary: str = ""
+    name: str = ""
+    numeric: str = ""
+    paper: str = ""
+    telephone: str = ""
+    time: str = ""
 
-        self.lang = lang or address
-        self.address = address or lang
-        self.identification = identification or address
-        self.measurement = measurement or address
-        self.monetary = monetary or address
-        self.name = name or address
-        self.numeric = numeric or address
-        self.paper = paper or address
-        self.telephone = telephone or address
-        self.time = time or address
+    def __post_init__(self):
+        default = self.address or "en_US.UTF-8"
+
+        self.lang = self.lang or default
+        self.address = self.address or default
+        self.identification = self.identification or default
+        self.measurement = self.measurement or default
+        self.monetary = self.monetary or default
+        self.name = self.name or default
+        self.numeric = self.numeric or default
+        self.paper = self.paper or default
+        self.telephone = self.telephone or default
+        self.time = self.time or default
 
 
+@dataclass
 class User:
-    def __init__(
-        self,
-        username: str = "",
-        fullname: str = "",
-        home_dir: str = "",
-        shell: str = "",
-        manage_home: bool = False,
-        groups: list[str] = [],
-    ):
-        if not username and not fullname:
-            fullname = "User"
+    username: str = ""
+    fullname: str = ""
+    home_dir: str = ""
+    shell: str = ""
+    manage_home: bool = False
+    groups: list[str] = field(default_factory=list)
 
-        self.username = username if username else fullname.replace(" ", "-").lower()
-        self.fullname = fullname if fullname else username
-        self.home_dir = home_dir if home_dir else f"/home/{username}"
-        self.shell = shell or "nu"
-        self.manage_home = manage_home or False
-        self.groups = groups
+    def __post_init__(self):
+        if not self.username and not self.fullname:
+            self.fullname = "User"
+
+        self.username = (
+            self.username if self.username else self.fullname.replace(" ", "-").lower()
+        )
+        self.fullname = self.fullname if self.fullname else self.username
+        self.home_dir = self.home_dir if self.home_dir else f"/home/{self.username}"
+        self.shell = self.shell or "nu"
+        self.manage_home = self.manage_home or False
 
         if not self.username in self.groups:
-            self.groups = [username] + self.groups
+            self.groups = [self.username] + self.groups
 
 
+@dataclass
 class Config:
-    def __init__(
-        self,
-        hostname: str = "",
-        timezone: str = "",
-        locale: Locale = Locale(),
-        kernels: list[str] = [],
-        users: list[User] = [],
-        bootloader: str = "",
-        initramfs_generator: str = "",
-        pre_build: str = "",
-        post_build: str = "",
-    ):
-        self.hostname = hostname.replace(" ", "-").lower() or "my-pc"
-        self.timezone = timezone or "Etc/UTC"
-        self.locale = locale or Locale()
-        self.kernels = kernels or ["linux"]
-        self.users = users or [User()]
-        self.bootloader = bootloader.lower() or "grub"
-        self.initramfs_generator = initramfs_generator.lower() or "dracut"
-        self.pre_build = pre_build
-        self.post_build = post_build
+    hostname: str = ""
+    timezone: str = ""
+    locale: Locale = field(default_factory=Locale)
+    kernels: list[str] = field(default_factory=list)
+    users: list[User] = field(default_factory=list)
+    bootloader: str = ""
+    initramfs_generator: str = ""
+    pre_build: str = ""
+    post_build: str = ""
+
+    def __post_init__(self):
+        self.hostname = self.hostname.replace(" ", "-").lower() or "my-pc"
+        self.timezone = self.timezone or "Etc/UTC"
+        self.locale = self.locale or Locale()
+        self.kernels = self.kernels or ["linux"]
+        self.users = self.users or [User()]
+        self.bootloader = self.bootloader.lower() or "grub"
+        self.initramfs_generator = self.initramfs_generator.lower() or "dracut"
+
+    def to_dict(self):
+        return {
+            "hostname": self.hostname,
+            "timezone": self.timezone,
+            "locale": asdict(self.locale),
+            "kernels": self.kernels,
+            "users": [asdict(user) for user in self.users],
+            "bootloader": self.bootloader,
+            "initramfs_generator": self.initramfs_generator,
+            "pre_build": self.pre_build,
+            "post_build": self.post_build,
+        }
 
     def emit(self):
-        locale = asdict(
-            _LocaleConfig(
-                self.locale.lang,
-                self.locale.address,
-                self.locale.identification,
-                self.locale.measurement,
-                self.locale.monetary,
-                self.locale.name,
-                self.locale.numeric,
-                self.locale.paper,
-                self.locale.telephone,
-                self.locale.time,
-            )
-        )
-        users = []
-        for user in self.users:
-            user_config = _UserConfig(
-                user.username,
-                user.fullname,
-                user.home_dir,
-                user.shell,
-                user.manage_home,
-                user.groups,
-            )
-            users.append(user_config)
-
-        config = _SystemConfig(
-            self.hostname,
-            self.timezone,
-            locale,
-            self.kernels,
-            users,
-            self.bootloader,
-            self.initramfs_generator,
-            self.pre_build,
-            self.post_build,
-        )
+        config = self.to_dict()
 
         nest_autogen = getenv("NEST_AUTOGEN") or ""
 
@@ -162,6 +100,6 @@ class Config:
             Path(nest_autogen).mkdir(parents=True)
 
         with open(path.join(nest_autogen, "config.json"), "w") as file:
-            dump(asdict(config), file, indent=4)
+            dump(config, file, indent=4)
 
-        print(dumps(asdict(config), indent=4))
+        print(dumps(config, indent=4))
